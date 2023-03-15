c1c6f0b9606c4aca0d14505ee30a5153721051cf
CarloMicieli <carlo.micieli@gmail.com>
2023-03-15 18:32:13 +0100

    build: init the gradle build configuration

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.17.0")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
+    implementation("me.qoomon:gradle-git-versioning-plugin:6.4.2")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.4")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
+    implementation("io.micronaut.gradle:micronaut-gradle-plugin:3.7.5")
+    implementation("io.micronaut.gradle:micronaut-test-resources-plugin:3.7.5")
+    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.10")
+}
