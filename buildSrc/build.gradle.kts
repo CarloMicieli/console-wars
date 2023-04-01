2e7abb7fc8fd83b55cb6813fe02032d6616e42f8
CarloMicieli <carlo.micieli@gmail.com>
2023-04-01 07:25:09 +0200

    build: update to spring boot 3.0.5

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.17.0")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
     implementation("me.qoomon:gradle-git-versioning-plugin:6.4.2")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.4")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.5")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
     implementation("io.micronaut.gradle:micronaut-gradle-plugin:3.7.5")
     implementation("io.micronaut.gradle:micronaut-test-resources-plugin:3.7.5")
