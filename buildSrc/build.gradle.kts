654e5baa57f3c478726f8a8f1f51b9b064c07e0a
CarloMicieli <carlo.micieli@gmail.com>
2023-06-26 20:02:39 +0200

    build: upgrade gradle plugin versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,14 +7,14 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.17.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.19.0")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
     implementation("me.qoomon:gradle-git-versioning-plugin:6.4.2")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.1")
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.47.0")
     implementation("io.micronaut.gradle:micronaut-gradle-plugin:3.7.10")
     implementation("io.micronaut.gradle:micronaut-test-resources-plugin:3.7.10")
-    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.20")
+    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.23")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.22")
 }
